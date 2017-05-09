ea608d75d30c24507c37731ac374d90c96ffb961
glocon <glocon@nike>
2017-05-09 13:41:05 -0700

    update 9/5/17

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -3,19 +3,19 @@ package com.thoughtmechanix.licenses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import java.util.Collections;
-import java.util.List;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+@EnableDiscoveryClient  //Inietta EurekaDiscoveryClient in DiscoveryClient in OrganizationDiscoveryClient
+
+@EnableFeignClients     //Inietta OrganizationFeignClient in LicenseService.Vedi anche @FeignClient in OrganizationFeignClient
 public class Application {
 
-  @LoadBalanced
+  @LoadBalanced         //Inietta RestTemplate  in  OrganizationRestTemplateClient configurando un LoadBalancerInterceptor 
   @Bean
   public RestTemplate getRestTemplate(){
       return new RestTemplate();
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -2,6 +2,8 @@ package com.thoughtmechanix.licenses.clients;
 
 import com.thoughtmechanix.licenses.model.Organization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,16 +11,27 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class OrganizationRestTemplateClient {
+	
+	/*@EnableDiscoveryClient abilita Ribbon intercepton nelle istanze  
+	 * RestTemplate managed da Spring!!
+	 * Ego la url dell organizzationservice e' automaticamente trovato!!
+	 *   "http://organizationservice/v1/organizations/{organizationId}" diventa a runtime 
+	 *   http://10.195.171.96:8080/v1/organizations/442adb6e-fa58-47f3-9ca2-ed1fecdfe86c
+	 */
+	
+	
     @Autowired
     RestTemplate restTemplate;
 
     public Organization getOrganization(String organizationId){
-        ResponseEntity<Organization> restExchange =
+        ResponseEntity<Organization> restExchange =  //Invia GET verso http://10.195.170.155:8080/v1/organizations/442adb6e-fa58-47f3-9ca2-ed1fecdfe86c
                 restTemplate.exchange(
-                        "http://organizationservice/v1/organizations/{organizationId}",
-                        HttpMethod.GET,
-                        null, Organization.class, organizationId);
-
+                        "http://organizationservice/v1/organizations/{organizationId}",  //String url
+                        HttpMethod.GET,    												 //HttpMethod method,
+                        null, 															 //HttpEntity<?> requestEntity
+                        Organization.class,                                              //Class<T> responseType
+                        organizationId); 												 //Object... uriVariables)
+       
         return restExchange.getBody();
     }
 }
